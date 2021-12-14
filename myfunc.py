6d6b8318c5fc175cbf7c0b01b969507b67f21caa
shoutaro_m <shoutaro.miura@gmail.com>
2021-12-14 13:04:08 +0900

    update myfunc.py

--- myfunc.py
+++ myfunc.py
@@ -22,29 +22,29 @@ from typing import List
 class CdAndLs:
     """Change Directory And List All Components.
 
-    cd した後に ls するために必要な属性とメソッドを定義します。
+    cd and ls するために必要な属性とメソッドを定義します。
 
     Attributes:
+        self.dir_title (str): ディレクトリ一覧のタイトル。
+        self.files_title (str): ファイル一覧のタイトル。
         new_path (str): 移動先のパスを格納する。
         self.dirs List[str]: ディレクトリ名の一覧を格納する変数。
         self.files List[str]: ファイル名の一覧を格納する変数。
-        self.dir_title (str): ディレクトリ一覧のタイトル。
-        self.files_title (str): ファイル一覧のタイトル。
+        self.str_len (int): 全角を含んだ文字列の文字数を格納する変数。
         self.dirs_list (str): ディレクトリ一覧を表示するための文字列を格納する変数。
         self.files_list (str): ファイル一覧を表示するための文字列を格納する変数。
-        self.str_len (int): 全角を含んだ文字列の文字数を格納する変数。
 
     """
 
     def __init__(self):
+        self.dir_title: str = '\n' + '------- directories -------'.center(52) + '\n'
+        self.files_title: str = '\n\n' + '------- files -------'.center(52) + '\n'
         self.new_path: str = ''
         self.dirs: List[str] = []
         self.files: List[str] = []
-        self.dir_title: str = '\n' + '------- directories -------'.center(52) + '\n'
-        self.files_title: str = '\n\n' + '------- files -------'.center(52) + '\n'
+        self.str_len: int = 0
         self.dirs_list: str = ''
         self.files_list: str = ''
-        self.str_len: int = 0
 
     def change_dir(self, path: str) -> None:
         """ディレクトリの移動。
@@ -64,7 +64,7 @@ class CdAndLs:
         カレントディレクトリのディレクトリ名とファイル名を全て取得し、アルファベット順にソートします。
 
         """
-        all_items: List[str] = os.listdir(self.new_path)  # ディレクトリ名とファイル名の全てを取得する。
+        all_items: List[str] = os.listdir(self.new_path)
 
         dirs: List[str] = [dir_name for dir_name in all_items
                            if os.path.isdir(os.path.join(self.new_path, dir_name))]  # ディレクトリ名のみを取得する。
@@ -78,7 +78,7 @@ class CdAndLs:
         self.dirs, self.files = dirs, files
 
     def fullwidth_contains_judge(self, string: str) -> bool:
-        """文字列中に全角文字かの判断
+        """文字列中に全角文字かの判断。
 
         引数に渡した文字列に全角文字が含まれるか判断します。
         全角文字が含まれる場合は、その文字を半角2文字分として文字数をカウントします。
@@ -96,7 +96,7 @@ class CdAndLs:
         for char in string:
             name = unicodedata.name(char)
 
-            if ("CJK UNIFIED" in name) or ("HIRAGANA" in name) or ("KATAKANA" in name):
+            if ('CJK UNIFIED' in name) or ('HIRAGANA' in name) or ('KATAKANA' in name):
                 count = count + 2  # 全角文字は2文字としてカウントする。
                 contains_fullwidth = True
 
@@ -106,47 +106,58 @@ class CdAndLs:
         self.str_len = count
         return contains_fullwidth
 
-    def add_blank(self):
-        """ リストの各要素に対して、文字数が38文字になるように空白文字を追加するメソッド """
+    def align_width(self) -> None:
+        """ディレクトリ名およびファイル名の文字数を揃える。
 
-        # ディレクトリ名について
-        for index, dir_name in enumerate(self.dirs):
-            dir_name = dir_name + "/"
+        各ディレクトリ名およびファイル名に対して、文字数が38文字になるように空白文字を追加します。
+
+        """
+        for index, dir_name in enumerate(self.dirs):  # ディレクトリ名について
+            dir_name = dir_name + '/'
             if self.fullwidth_contains_judge(dir_name):
-                self.dirs[index] = dir_name + " " * (38 - self.str_len)  # 全角文字が含まれる場合
+                self.dirs[index] = dir_name + ' ' * (38 - self.str_len)  # 全角文字が含まれる場合
             else:
                 self.dirs[index] = dir_name.ljust(38)  # 半角文字のみの場合
 
-        # ファイル名について
-        for index, file_name in enumerate(self.files):
+        for index, file_name in enumerate(self.files):  # ファイル名について
             if self.fullwidth_contains_judge(file_name):
-                self.files[index] = file_name + " " * (38 - self.str_len)  # 全角文字が含まれる場合
+                self.files[index] = file_name + ' ' * (38 - self.str_len)  # 全角文字が含まれる場合
             else:
                 self.files[index] = file_name.ljust(38)  # 半角文字のみの場合
 
-    def arrange_list(self):
-        """ リスト内の各要素を2個ずつ並べて一覧表示になるように整形する。 """
+    def create_display_str(self) -> None:
+        """表示する文字列の作成。
 
-        dis_len = len(self.dirs)
-        files_len = len(self.files)
+        リスト内の各要素を2個ずつ並べて、一覧表示するための文字列を作成します。
 
-        dirs_list = ""  # 整形した文字列を格納する。(ディレクトリ名)
-        files_list = ""  # 整形した文字列を格納する。(ファイル名)
+        """
+        dirs_len, files_len = len(self.dirs), len(self.files)
+        dirs_list, files_list, = '', ''  # 作成した文字列を格納する。
 
         # ディレクトリ名について
-        for i in range(0, dis_len - 1, 2):  # 2個ずつ並べる。
-            dirs_list = dirs_list + self.dirs[i] + self.dirs[i + 1] + "\n"
+        for i in range(0, dirs_len - 1, 2):  # 2個ずつ並べる。
+            dirs_list = dirs_list + self.dirs[i] + self.dirs[i + 1] + '\n'
 
-        if self.dirs and (dis_len % 2):  # 奇数個(0でない)の場合
+        if self.dirs and (dirs_len % 2):  # 奇数個 (0でない) の場合。
             dirs_list = dirs_list + self.dirs[-1]
-
-        self.dirs_list = dirs_list
 
         # ファイル名について
         for i in range(0, files_len - 1, 2):  # 2個ずつ並べる。
-            files_list = files_list + self.files[i] + self.files[i + 1] + "\n"
+            files_list = files_list + self.files[i] + self.files[i + 1] + '\n'
 
-        if self.files and (files_len % 2):  # 奇数個(0でない)の場合
+        if self.files and (files_len % 2):  # 奇数個 (0でない) の場合。
             files_list = files_list + self.files[-1]
 
-        self.files_list = files_list
+        self.dirs_list, self.files_list = dirs_list, files_list
+
+    def main(self, path):
+        """メインメソッド。
+
+        上記で定義したメソッドを適切な順序で呼び出します。
+        .xonshrc でこのメソッドを呼び出してください。
+
+        """
+        self.change_dir(path)  # 指定したパスへ移動する。
+        self.get_items()  # ディレクトリ名とファイル名を全て取得する。
+        self.align_width()  # 幅が半角で38文字分になるように空白を追加する。
+        self.create_display_str()  # 2列ずつに並べて文字列を作成する。
